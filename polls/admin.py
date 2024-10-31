22ebe0f0c677f2f7558b82c057657d9bd58ec566
SHUSHU0925 <emo11280925@gmail.com>
2024-10-31 12:22:33 +0900

    polls/admin.pyのクラス変更から

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+
 # Register your models here.
